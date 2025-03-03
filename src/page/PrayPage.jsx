794cd81d0c370c08e57d9284f906ede447301e72
areandra <muhammadariel2207@gmail.com>
2025-03-03 11:48:55 +0800

    Initial Commit

--- src/page/PrayPage.jsx
+++ src/page/PrayPage.jsx
@@ -1,14 +1,36 @@
 import '../page/PrayPage.css'
+import { useState } from 'react';
+import { motion } from 'framer-motion';
+
+
 
 function PrayPage() {
+  const [isOpen, setIsOpen] = useState (false);
+  const PrayBook = () => {
+    return (
+      <motion.div
+        initial={{ opacity: 0, y: 300 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 2, ease: "easeOut" }}
+      > <div className="container">
+          <div className={`book ${isOpen ? "open" : ""}`} onClick={() => setIsOpen(!isOpen)}>
+            <div className="cover">Klik untuk Buka</div>
+            <div className="page"></div>
+          </div>
+        </div>
+      </motion.div>
+    )
+  }
   return (
     <>
         <section id="PrayPage">
             <div>
                 <h1>Doa</h1>
             </div>
+            <PrayBook />
         </section>
     </>
   );
 }
 export default PrayPage
+
